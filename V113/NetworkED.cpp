ca29249fae8ca2ec35bad134ff84d2dd9dbdb59b
javier <javier@Jarvis>
2017-01-18 17:51:57 -0300

    Added client init routine // without testing

--- V113/NetworkED.cpp
+++ V113/NetworkED.cpp
@@ -34,7 +34,7 @@ CharacterName NetworkED::getCharacter()
     CharacterName retVal;
     if(header==I_AM)
     {
-        retVal=(CharacterName)buffer[0];
+        retVal=(CharacterName)buffer[0];        //Su primer dato como numero representa justo el character name
     }
     else 
         error=true;
@@ -44,7 +44,7 @@ CharacterName NetworkED::getCharacter()
 string NetworkED::getName()
 {
     buffer[len]= '\0';
-    string aux = (char *)(&(buffer[1]));
+    string aux = (char *)(&(buffer[1]));    //Como en el paquete name is se guarda los chars a partir del 2do byte y en el primero su largo es válido esto.
     return aux;
 }
 
@@ -53,8 +53,8 @@ void NetworkED::getInitGPos(CardLocation *guardPos, CardLocation *guardsDiePos)
     if(header==INITIAL_G_POS)
     {
         buffer[len]= '\0';
-        string aux = (char *)buffer;
-        *guardPos=protocolToCardLocation(aux.substr(0,PROTOCOL_LOCATION_LENGTH));
+        string aux = (char *)buffer;    //Aca quedaría en aux por ejemplo A1F2A2F1
+        *guardPos=protocolToCardLocation(aux.substr(0,PROTOCOL_LOCATION_LENGTH));   //Acá se los separa.
         *guardsDiePos=protocolToCardLocation(aux.substr(PROTOCOL_LOCATION_LENGTH,PROTOCOL_LOCATION_LENGTH));
     }
     else 
@@ -68,8 +68,8 @@ void NetworkED::getStartInfo(vector<CardName> *tiles, CardLocation *playersStart
         buffer[len]= '\0';
         unsigned int i;
         for(i=0;i<BOARD_STANDARD_FLOORS*FLOOR_RAWS*FLOOR_COLUMNS; i++)
-            tiles->push_back((CardName)buffer[i]);
-        string aux= (char *)(&(buffer[i]));
+            tiles->push_back((CardName)buffer[i]);      //Se pasa el tipo de cada tile
+        string aux= (char *)(&(buffer[i]));             //Luego la posición el protocolo se obtiene al finalizar los tiles
         *playersStartingPos=protocolToCardLocation(aux);
     }
     else 
